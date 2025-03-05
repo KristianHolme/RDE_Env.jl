b372fb250f9b25c5a73c1c0b224b7b04b1891f31
Kristian Holme <holme.kristian@gmail.com>
2025-03-05 21:27:35 +0100

    test of upwind

--- examples/stepwise_control.jl
+++ examples/stepwise_control.jl
@@ -2,10 +2,12 @@ using RDE
 using RDE_Env
 using GLMakie
 
-env = RDEEnv(dt=0.1f0);
+params = RDEParam(tmax=500f0, N=2048)
+env = RDEEnv(params, method=UpwindMethod(order=2),
+    action_type=ScalarAreaScalarPressureAction());
 π = StepwiseRDEPolicy(env, [20.0f0, 100.0f0, 200.0f0, 350.0f0], 
 [[3.5f0, 0.64f0], [3.5f0, 0.86f0], [3.5f0, 0.64f0], [3.5f0, 0.94f0]]);
 data = run_policy(π, env)
 fig = plot_policy_data(env, data)
-
+##
 animate_policy_data(data, env; fname="stepwise_control", fps=60)
