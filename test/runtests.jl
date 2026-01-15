4e5a167a24a82e8100645c1210de22e784cc80cd
Kristian Holme <holme.kristian@gmail.com>
2026-01-15 18:39:35 +0100

    add aqua and JET tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,17 @@
+using RDE_Env
+using Test
+using TestItems
 using TestItemRunner
+
+@testitem "Code quality (Aqua.jl)" tags = [:quality] begin
+    using Aqua, RDE_Env
+    Aqua.test_all(RDE_Env)
+end
+
+@testitem "Code linting (JET.jl)" tags = [:quality] begin
+    using JET, RDE_Env
+    report = JET.report_package(RDE_Env; target_modules = (RDE_Env,), toplevel_logger = nothing)
+    @test isempty(JET.get_reports(report))
+end
 
 @run_package_tests
